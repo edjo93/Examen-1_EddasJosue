2244b573a0ed49ae94ba227e64f79dc17e87a0c5
edjo93 <eddas.carrasco@unitec.edu>
2019-05-17 13:37:17 -0600

    prototipos y definiciones de funcion  para matriz dinamica  agregados

--- examen1.cpp
+++ examen1.cpp
@@ -3,6 +3,12 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//prototipos de funcion
+int** provisionar_matriz(int);
+void imprimir_matriz(int**,int);
+void liberar_matriz(int**,int);
+
+
 int main(){
         //menu con fin centinela
         //mientras no se seleccione la opcion de salida el menu continua
@@ -27,4 +33,41 @@ int main(){
         
         
         
+}
+
+//definicion de funciones
+int** provisionar_matriz(int size){
+	int**matriz=NULL;
+
+	matriz=new int*[size];//reservando memoria lara las filas
+
+	for(int i=0;i<size;i++){
+		matriz[i]=new int[size];//reservando memoria para las columnas
+	}
+
+	return matriz;
+}
+void imprimir_matriz(int** matriz,int size){
+	for (int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			cout<<"["<<matriz[i][j]<<"]";
+		}
+		cout<<endl;
+	}
+}
+void liberar_matriz(int** matriz,int size){
+	for(int i=0;i<size;i++){
+		if(matriz[i]!=NULL){
+			delete[] matriz[i];
+			matriz[i]=NULL;
+
+		}
+	}	
+	
+
+
+	if(matriz!=NULL){
+		delete[] matriz;
+		matriz=NULL;
+	}
 }
